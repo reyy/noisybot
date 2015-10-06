73872c33d426ccefb991bb3090d8a5c46b3b9161
Rey Neo <rey@live.jp>
2015-10-07 01:45:38 +0800

    Use existing image/audio files on server instead of reuploading each time

--- bot.js
+++ bot.js
@@ -3,6 +3,11 @@ var http = require('http');
 var xpath = require('xpath');
 var dom = require('xmldom').DOMParser;
 
+const BOT_FILES = {
+	MORNING : 'AgADBQADu6cxG6gBDgdA2w9yAugWKTiyszIABDDnqj0Hlyn6bfEAAgI',
+	GREETINGS : 'AwADBQADBAADqAEOBzK8hOwVpUFpAg'
+}
+
 var token = process.env.OPENSHIFT_TELEGRAM_TOKEN;
 var port = process.env.OPENSHIFT_NODEJS_PORT;
 var host = process.env.OPENSHIFT_NODEJS_IP;
@@ -15,21 +20,19 @@ bot.on('message', function (msg) {
     var cmd = msg.text.split(" ");
     var chatId = msg.chat.id;
     if (cmd[0] == '/morning' || cmd[0] == '/morning@NoisyBot') {
-        var photo = 'morning.jpg';
-        bot.sendPhoto(chatId, photo, { caption: "Noisy bot says HAI!" });
+        bot.sendPhoto(chatId, BOT_FILES.MORNING, { caption: "Noisy bot says Good Morning!"});
     }
     else if (cmd[0] == '/greetings' || cmd[0] == '/greetings@NoisyBot') {
-        var voice = 'greetings.ogg';
-        bot.sendVoice(chatId, voice);
+        bot.sendAudio(chatId, BOT_FILES.GREETINGS);
     }
     else if (cmd[0] == '/hello' || cmd[0] == '/hello@NoisyBot') {
         bot.sendMessage(chatId, 'HAI guys!');
     }
-    else if (cmd[0] == '/help' || cmd[0] == '/help@NoisyBot') {
+    else if (cmd[0] == '/help' || cmd[0] == '/help@NoisyBot' || cmd[0] == '/start') {
         var opts = {
             reply_to_message_id: msg.message_id
         };
-        bot.sendMessage(chatId, 'Type /hello or /morning 😁');
+        bot.sendMessage(chatId, 'Type / to see the commands available! 😁');
     }
     else if (cmd[0] == '/psi' || cmd[0] == '/psi@NoisyBot') {
         try {
